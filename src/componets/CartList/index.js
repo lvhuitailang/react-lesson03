5a30ef43dd3e4f832b5351f74dda6e373f1829c8
wolfie <1039719274@qq.com>
2020-06-16 17:24:31 +0800

    use react-redux

--- src/componets/CartList/index.js
+++ src/componets/CartList/index.js
@@ -2,29 +2,31 @@ import React, {Component} from 'react';
 
 import {increament,decreament} from '../../actions/cart'
 
+import {connect} from 'react-redux'
+
 class CartList extends Component {
     constructor() {
         super();
-        this.state = {
+     /*   this.state = {
             cartList:[]
-        }
+        }*/
     }
 
-    listenerSetState = ()=>{
+/*    listenerSetState = ()=>{
         this.setState(()=>{
             return {
-                cartList:this.props.store.getState().cart
+                cartList:this.props.cartList
             }
         })
     }
 
     componentDidMount() {
       this.listenerSetState();
-      this.props.store.subscribe(this.listenerSetState);
-    }
+      // this.props.store.subscribe(this.listenerSetState);
+    }*/
 
     render() {
-        console.log('rendered')
+        console.log(this.props)
         return (
             <>
              <table>
@@ -39,23 +41,15 @@ class CartList extends Component {
                  </thead>
                  <tbody>
                  {
-                     this.state.cartList.map(item=>{
+                     this.props.cartList.map(item=>{
                          return <tr key={item.id}>
                                      <td>{item.id}</td>
                                      <td>{item.title}</td>
                                      <td>{item.price}</td>
                                      <td>
-                                         <button onClick={
-                                             ()=>{
-                                                 this.props.store.dispatch(decreament(item.id));
-                                             }
-                                         }>-</button>
+                                         <button onClick={this.props.reduce.bind(this,item.id)}>-</button>
                                          <span>{item.amount}</span>
-                                         <button onClick={
-                                             ()=>{
-                                                 this.props.store.dispatch(increament(item.id));
-                                             }
-                                         }>+</button>
+                                         <button onClick={this.props.add.bind(this,item.id)}>+</button>
                                      </td>
                                      <td></td>
                                 </tr>
@@ -69,4 +63,21 @@ class CartList extends Component {
     }
 }
 
-export default CartList;
+const mapStateToProp = (state)=>{
+    return {
+        cartList:state.cart
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        add:(id)=>{
+            return dispatch(increament(id))
+        },
+        reduce:(id)=>{
+            return dispatch(decreament(id))
+        }
+    }
+}
+
+export default connect(mapStateToProp,mapDispatchToProps)(CartList);
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -15,6 +15,29 @@ const initState = [{
 
 export default (state = initState,action)=>{
     console.log(state)
+    switch (action.type) {
+        case actionType.CART_AMOUNT_INCREASEMENT:
+            return state.map(item=>{
+                if(item.id === action.payload.id){
+                    item.amount+=1;
+                }
+                return item;
+            })
+        case actionType.CART_AMOUNT_DECREASEMENT:
+            return state.map(item=>{
+                if(item.id === action.payload.id){
+                    item.amount-=1;
+                }
+                return item;
+            })
+        default:return state;
+    }
+
+}
+/*
+//js里面 foreach filter等等，不能改变原来数组的引用地址，只能改变每个item里面的值
+export default (state = initState,action)=>{
+    console.log(state)
     let tempItem = {};
     if(action && action.type && action.payload){
         let tempArrState = state.filter(v=>{
@@ -31,7 +54,7 @@ export default (state = initState,action)=>{
         case actionType.CART_AMOUNT_DECREASEMENT:
             tempItem.amount =  tempItem.amount-1;
             return state;
-        default:return initState;
+        default:return state;
     }
 
-}
+}*/
