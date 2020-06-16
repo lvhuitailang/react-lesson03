bd27458835bc629e1163c92e590ca6cf2aa5f1c4
wolfie <1039719274@qq.com>
2020-06-16 17:57:55 +0800

    1

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -15,23 +15,22 @@ const initState = [{
 
 export default (state = initState,action)=>{
     console.log(state)
-    let tempItem = {};
-    if(action && action.type && action.payload){
-        let tempArrState = state.filter(v=>{
-            return v.id === action.payload.id
-        });
-        if(tempArrState && tempArrState.length > 0){
-            tempItem = tempArrState[0];
-        }
-    }
     switch (action.type) {
         case actionType.CART_AMOUNT_INCREASEMENT:
-            tempItem.amount =  tempItem.amount+1;
-            return state;
+            return state.map(item=>{
+                if(item.id === action.payload.id){
+                    item.amount+=1;
+                }
+                return item;
+            })
         case actionType.CART_AMOUNT_DECREASEMENT:
-            tempItem.amount =  tempItem.amount-1;
-            return state;
-        default:return initState;
+            return state.map(item=>{
+                if(item.id === action.payload.id){
+                    item.amount-=1;
+                }
+                return item;
+            })
+        default:return state;
     }
 
 }
