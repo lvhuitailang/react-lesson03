e0e28b9454a471b113295c117dcd576c0256f84f
wolfie <1039719274@qq.com>
2020-06-17 19:03:23 +0800

    1

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -12,6 +12,43 @@ const initState = [{
         amount:5
     }];
 
+export default (state = initState,action)=>{
+    console.log(state)
+    //这里拷贝一份，因为必须返回一个新的数据，不能是原来的引用
+    let newState = JSON.parse(JSON.stringify(state));
+    let tempItem = {};
+    if(action && action.type && action.payload){
+        let tempArrState = newState.filter(v=>{
+            return v.id === action.payload.id
+        });
+        if(tempArrState && tempArrState.length > 0){
+            tempItem = tempArrState[0];
+        }
+    }
+    switch (action.type) {
+        case actionType.CART_AMOUNT_INCREASEMENT:
+            tempItem.amount+=1;
+            break;
+        case actionType.CART_AMOUNT_DECREASEMENT:
+            tempItem.amount-=1;
+            break;
+        default:return newState;
+    }
+    return newState;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 export default (state = initState,action)=>{
     console.log(state)
@@ -34,6 +71,7 @@ export default (state = initState,action)=>{
     }
 
 }
+*/
 
 /*
 //js里面 foreach filter等等，不能改变原来数组的引用地址，只能改变每个item里面的值
