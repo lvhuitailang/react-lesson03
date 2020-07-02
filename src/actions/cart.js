1f643cd98f0f68def8f0b16a5688795067cbf3db
wolfie <1039719274@qq.com>
2020-07-02 10:58:34 +0800

    q

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -18,15 +18,20 @@ export const decreament = (id)=>{
     }
 
 }
+
+//异步action，使用redux-thunk之后就可以在这里return 一个function，参数是dispach
+/*
 export const decreamentAsync = (id)=>{
+    return(dispach)=>{
+        setTimeout(()=>{
+            dispach (decreament(id));
+        },2000)
+    }
+
+}*/
+
+export const decreamentAsync = id => dispach => {
     setTimeout(()=>{
-        return {
-            type:actionType.CART_AMOUNT_DECREASEMENT,
-            payload:{
-                id:id
-            }
-        }
+        dispach (decreament(id));
     },2000)
-
-
 }
