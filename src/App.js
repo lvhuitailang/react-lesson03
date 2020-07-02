6c50ac7bb0cd42003c10ddbf56a37deccb4c6732
wolfie <1039719274@qq.com>
2020-07-02 18:07:51 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 
+import {
+    Home,
+    Article,
+    Users
+}from './views'
+
+
 class App extends Component {
     render() {
         return (
             <div>
-                App
-                
+                <ul>
+                    <li><a href={''} >首页</a> </li>
+                    <li><a href={''} >文章</a> </li>
+                    <li><a href={''} >用户</a> </li>
+                </ul>
+                <Home/>
+                <Article/>
+                <Users/>
             </div>
         );
     }
---
+++ src/views/Article/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Article extends Component {
+    render() {
+        return (
+            <div>
+                article
+            </div>
+        );
+    }
+}
+
+export default Article;
