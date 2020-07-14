29fc67a239d8667478c2ab32e4f858ef19f7942b
wolfie <1039719274@qq.com>
2020-07-14 14:41:27 +0800

    a

--- src/views/Article/ArticleDetail.js
+++ src/views/Article/ArticleDetail.js
@@ -2,10 +2,20 @@ import React, {Component} from 'react';
 
 
 class ArticleDetail extends Component {
+    backtohome = ()=>{
+        this.props.history.push({
+            pathname:'/home',
+            state:{
+                id:this.props.match.params.id
+            }
+        });
+    }
     render() {
+        console.log(this.props)
         return (
             <div>
                 文章{this.props.match.params.id}
+                <button onClick={this.backtohome}>返回首页</button>
             </div>
         );
     }
